c467d7ae3458afaca1039b8ae41d7f849d6b5bfc
Artur-Lepczynski <artur.lepczynski@gmail.com>
2023-11-16 21:15:54 +0100

    add: the search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import SearchBar from "./components/UI/SearchBar";
 
-function App() {
+export default function App() {
+  const [searchValue, setSearchValue] = useState("");
+
+  function handleSearch(searchValue) {
+    console.log(searchValue);
+    setSearchValue(searchValue);
+  }
+
+  function handleClear() {
+    console.log("Cleared!");
+    setSearchValue("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <SearchBar onSearch={handleSearch} onClear={handleClear} />
+      <p>
+        {searchValue
+          ? `You're searching for ${searchValue}`
+          : "You're not searching for anything"}
+      </p>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/UI/SearchBar.js
@@ -0,0 +1,61 @@
+import style from "./SearchBar.module.css";
+import { useRef, useState } from "react";
+
+export default function SearchBar(props) {
+  const [enteredSearchValue, setEnteredSearchValue] = useState("");
+
+  const inputRef = useRef(null); 
+  const [inputIsFocused, setInputIsFocused] = useState(false);
+
+  function handleFocus(){
+    setInputIsFocused(true);
+  }
+
+  function handleBlur(){
+    setInputIsFocused(false);
+  }
+
+  function handleEnterPress(event){
+    if(event.key === "Enter"){
+      handleSearch();
+    }
+  }
+
+  function handleChange(event) {
+    setEnteredSearchValue(event.target.value);
+  }
+
+  function handleSearch() {
+    if (enteredSearchValue.trim().length === 0) return;
+    props.onSearch(enteredSearchValue);
+  }
+
+  function handleClear() {
+    setEnteredSearchValue("");
+    inputRef.current.focus();
+    props.onClear();
+  }
+
+  return (
+    <div className={`${style.wrapper} ${inputIsFocused && style["input-focused"]}`}>
+      <input
+        className={style.input}
+        type="text"
+        onChange={handleChange}
+        value={enteredSearchValue}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onKeyDown={handleEnterPress}
+        ref={inputRef}
+      />
+      {enteredSearchValue && (
+        <button className={style.button} type="button" onClick={handleClear}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
+      )}
+      <button className={style.button} type="button" onClick={handleSearch}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
+  );
+}
